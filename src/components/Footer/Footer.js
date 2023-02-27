27d327660144e6fef61a1f339d1ff5bf7b71bd6d
SssngM <rarang222@gmail.com>
2023-02-27 12:14:43 -0800

    add email form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { flushSync } from "react-dom";
 import "./Footer.css";
-import mail from "../../img/mail.png";
+import insta from "../../img/insta.png";
 import meetup from "../../img/meetup.png";
 
+
 function Footer() {
+  const handleClick = () => {
+    // implementation details
+  };
+
+  
   return (
     <div className="footer">
-      <p> Contact</p>
-      <div className="f-dec">Get in Touch</div>
+      <p> Keep in touch</p>
+      {/* <div className="f-dec">Get in Touch</div> */}
       <div> Do you want to valunteer or donate? feel free to contact us!</div>
+      <div>
+      <button type="button" onClick={handleClick}>
+        <a href = "https://us12.list-manage.com/contact-form?u=c2fe686774abda43cf2247db9&form_id=ea57a290a2932a5f3b1d2216d8139c2b">
+        Contact us
+        </a>
+      </button>
+    </div>
       <div className="f-img">
-        <a href="mailto:techgirlssocial@gmail.com ">
-          <img src={mail} />
+        <a href="https://instagram.com/techgirlssocial?igshid=YmMyMTA2M2Y=">
+          <img src={insta} />
         </a>
 
         <a href="https://www.meetup.com/techgirlssocial/">
           <img src={meetup} />
         </a>
+        
       </div>
     </div>
   );
